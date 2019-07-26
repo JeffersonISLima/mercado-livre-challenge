67d0d960d3cad6ab3e81f5fa8c0fb0163b7c4e95
jeffersoninacio@hotmail.com <jeffersoninacio@hotmail.com>
2019-07-26 14:12:35 -0300

    API: Cors
    Instalada e configurada a dependência Cors que permitirá a comunicação da API com o front-end.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
-const createError = require('http-errors');
-const express = require('express');
+const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const express = require('express');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 
@@ -17,6 +19,12 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Cors
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'],
+}));
 
 // Routes
 const indexRouter = require('./routes/index');
